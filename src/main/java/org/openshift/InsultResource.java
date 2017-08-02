2e9eab0767bc9cf74445d16b762a3e4d5848e055
jan@rubix.nl <jan@rubix.nl>
2017-08-02 18:59:42 +0200

    added rest api

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -7,13 +7,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @RequestScoped @Path("/insult")
+
 public class InsultResource
 {
     @GET()
     @Produces("application/json")
     public HashMap<String,String> getInsult()
     {
-        HashMap<String, String> theInsult = new HashMap<String, String>(); theInsult.put("insult", new InsultGenerator().generateInsult()); return theInsult;
-
+        HashMap<String, String> theInsult = new HashMap<String, String>(); theInsult.put("insult", new InsultGenerator().generateInsult());
+        return theInsult;
     }
 }
